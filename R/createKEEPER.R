f030edbfd7c5a69b3d255e987c1b646a5b2e8b71
Anna Ostropolets <anna.ostropolets@odysseusinc.com>
2023-08-13 20:37:12 -0400

    visit context modification

--- R/createKEEPER.R
+++ R/createKEEPER.R
@@ -77,14 +77,20 @@
 #'   password = "secure"
 #' )
 #'
+#' XXX: to do: test for personIds, add windows as parameters, format as loop, check ranges for measurements, add support for cdi as list
 #' createKEEPER(
 #'   connectionDetails = connectionDetails,
 #'   cohortDefinitionId = 1234,
 #'   cohortName = "DM type I",
+#'   sampleSize = 100,
+#'   exportFolder = "c:/temp/keeper",
+#'   databaseId = "Synpuf",
+#'   assignNewId = TRUE,
+#'   measValues = TRUE,
 #'   PriorConditions = c(201820,442793,443238,4016045,4065354,45757392, 4051114, 433968, 375545, 29555009, 4209145, 4034964, 380834, 4299544, 4226354, 4159742, 43530690, 433736,
 #'                     320128, 4170226, 40443308, 441267, 4163735, 192963, 85828009),
 #'   PriorDrugs = c(1730370, 21604490, 21601682, 21601855, 21601462, 21600280, 21602728, 1366773, 21602689, 21603923, 21603746),
-#'   DiagnosticProcedures = "",
+#'   DiagnosticProcedures = c(40756884, 4143852, 2746768, 2746766),
 #'   Measurements	= c(3034962, 3000483, 3034962, 3000483, 3004501, 3033408, 3005131, 3024629, 3031266, 3037110, 3009261, 3022548, 3019210, 3025232, 3033819,
 #'                  3000845, 3002666, 3004077, 3026300, 3014737, 3027198, 3025398, 3010300, 3020399, 3007332, 3025673, 3027457, 3010084, 3004410, 3005673),
 #'   AlternativeDiagnosis = c(201820,442793,443238,4016045,4065354,45757392, 4051114, 433968, 375545, 29555009, 4209145, 4034964, 380834, 4299544, 4226354, 4159742, 43530690, 433736,
@@ -262,20 +268,8 @@ createKEEPER <- function(connectionDetails = NULL,
     ))
   }
 
-  #XXX remove
- 
- # rdsFileName <- paste0(
- #   "KEEPER_",
- #   abs(cohortDefinitionId),
- #   "_",
- #   databaseId,
- #   ".rds"
- # )
-
   # Set up connection to server ----------------------------------------------------
   
-
-  # XXX support cohort definition id as a list
 
   if (is.null(connection)) {
     if (!is.null(connectionDetails)) {
@@ -552,7 +546,6 @@ complications = complications%>%
   writeLines("Writing KEEPER file.")
 
 
-# XXXXX works weird with multiple strings in prior_conditions
   KEEPER = subjects%>%
   dplyr::left_join(presentation, by = "personId")%>%
   dplyr::left_join(visit_context, by = "personId")%>%
