852b3000b0ade83031962b4d6465109024c39afd
Anna Ostropolets <anna.ostropolets@odysseusinc.com>
2023-08-21 10:28:45 +0300

    pipe fix (#3) (#4)

--- R/createKEEPER.R
+++ R/createKEEPER.R
@@ -320,7 +320,7 @@ createKEEPER <- function(connectionDetails = NULL,
       progressBar = TRUE,
       bulkLoad = (Sys.getenv("bulkLoad") == TRUE),
       camelCaseToSnakeCase = TRUE,
-      data = dplyr::tibble(subjectId = as.double(personIds) |> unique())
+      data = dplyr::tibble(subjectId = as.double(personIds) %>% unique())
     )
 
     DatabaseConnector::renderTranslateExecuteSql(
