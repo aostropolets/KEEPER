fc15a24247b5e0bbb6b06ee837e63840a2efdaaf
Anna Ostropolets <anna.ostropolets@odysseusinc.com>
2023-09-04 19:01:00 -0400

    Update createKEEPER.R

--- R/createKEEPER.R
+++ R/createKEEPER.R
@@ -463,6 +463,7 @@ write.csv(presentation, "pr.csv")
 
  subjects = presentation%>%
  dplyr::select(personId, newId, age, gender, cohortDefinitionId, cohortStartDate, observationPeriod)%>%
+ dplyr::rename(observation_period = observationPeriod)
  dplyr::distinct()  
       
  presentation = presentation%>%
@@ -614,8 +615,8 @@ death = death%>%
   dplyr::left_join(after_drugs, by = c("personId", "cohortStartDate", "cohortDefinitionId"))%>%
   dplyr::left_join(after_treatment_procedures, by = c("personId", "cohortStartDate", "cohortDefinitionId"))%>%
   dplyr::left_join(death, by = c("personId", "cohortStartDate", "cohortDefinitionId"))%>%
-  dplyr::select(personId, newId, age, gender, observationPeriod, visitContext, presentation, symptoms, priorDisease, priorDrugs, priorTreatmentProcedures,
-                diagnosticProcedures, measurements, alternativeDiagnosis, afterDisease, afterTreatmentProcedures, afterDrugs, death)%>%
+  dplyr::select(personId, newId, age, gender, observation_period, visit_context, presentation, symptoms, prior_disease, prior_drugs, prior_treatment_procedures,
+                diagnostic_procedures, measurements, alternative_diagnosis, after_disease, after_treatment_procedures, after_drugs, death)%>%
   dplyr::distinct()%>%
   # add columns for review
   tibble::add_column(reviewer = NA, status = NA, index_misspecification = NA, notes = NA)
